3ac7c4593d8e476eb325770229f5ea16f35704ec
AsmodeusXI <AsmodeusXI@gmail.com>
2017-10-12 20:37:37 -0400

    feat: create random names, a whole list!
    Create a random name from a small set of syllables.
    Return a list of those random names that are not duplicates and are in
    order.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
-class App {
+const nameSyllables = ['akt', 'hel', 'thel', 'oss', 'fen'];
+const maxSyllables = 3;
+
+class NameGenerator { 
+ 
   public static createNames(): Array<String> {
-    console.log("Hello World!");
-    return [];  
+    let randomNames = 10;
+    let nameList:Array<String> = [];
+    while (randomNames > 0) {
+      let currentName = NameGenerator.createName();
+      if (!nameList.includes(currentName)) {
+        nameList.push(currentName);
+        randomNames--;
+      }
+    }
+    nameList.sort();
+    return nameList;
+  }
+
+  public static createName(): String {
+    let fullName = "";
+    let nameSize = NameGenerator.getRandomInteger(1, maxSyllables+1);
+    for (let i = 0; i < nameSize; i++) {
+      let syllableIdx = NameGenerator.getRandomInteger(0, nameSyllables.length);
+      fullName = `${fullName}${nameSyllables[syllableIdx]}`;
+    }
+    return fullName;
+  }
+
+  private static getRandomInteger(min:number, max:number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max-min)) + min;
   }
 }
 
-App.createNames();
+console.log(NameGenerator.createNames());
